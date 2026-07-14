dc3a5bfaed3b2ee11d33529e1d55bdd0ebba083d
Sachin Borude <sachinborude0610@gmail.com>
2026-07-14 22:44:39 +0530

    Practise Problem

---
+++ 01_string_slicing.py
@@ -0,0 +1,17 @@
+s = "nandini santosh kale"
+n = ['nandini', 'santosh', 'kale']
+print(len(s))
+print(s.upper())
+print(s.lower())
+print(s.capitalize())
+print(s.title())
+print(s.strip())
+print(s.split())
+print("".join(n))
+print(s.find("n"))
+print(s.count("s"))
+print(s.replace("nandini","rudra"))
+print(s.startswith("n"))
+print(s.endswith("e"))
+
+
